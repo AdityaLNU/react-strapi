4508915b07a7910da7cff645b6cec4f3218fc21e
Aditya LNU <t-adityalnu@microsoft.com>
2022-06-08 11:36:47 +0530

    :rocket: files added

---
+++ frontend-react/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import {useState, useEffect} from "react";
+const App = () => {
+
+  const [data, setData] = useState();
+
+  const apiCalling = async () =>{
+    const options = {
+      method:"GET",
+      url:`${process.env.REACT_APP_STRAPI_URL}/api/about`
+    }
+    const res = await axios.request(options);
+    setData(res.data.data.attributes.Title);
+    console.log(res.data)
+
+  }
+
+  useEffect(() =>{
+    apiCalling();
+  },[])
+  return (
+    <div className="App">
+      <h1> React Strapi example </h1>
+      {data ? (<p>Response from API: <em><strong>{data}</strong></em> </p>):"Yet to recieve"}
+    </div>
+  );
+}
+
+export default App;
